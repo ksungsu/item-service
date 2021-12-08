9a5a56f5e6c5aaa4654447ace364e9ab1cf85af1
ksungsu <ksungsu@naver.com>
2021-12-08 22:19:22 +0900

    TIL 47th day

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -18,12 +20,44 @@ public class BasicItemController {
 
     private final ItemRepository itemRepository;
 
+    /**
+     * itemRepository에 items 속성 추가
+     */
     @GetMapping
     public String items(Model model){
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "basic/items";
     }
+
+    /**
+     * 상품상세용 페이지 GetMapping
+     * 경로변수 PathVariable
+     */
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model){
+
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    /**
+     * 상품 등록 form(조회)
+     */
+    @GetMapping("/add")
+    public String addForm(){
+        return "basic/addForm";
+    }
+
+    /**
+     * 상품 등록
+     */
+    @PostMapping("/add")
+    public String save(){
+        return "basic/addForm";
+    }
+
 
     /**
      * 테스트용 데이터 추가
